e9a847dc304c10c05d8a3166931b6da8171ca754
adrixcpu <adriana.cpu@gmail.com>
2021-10-13 20:43:02 -0500

    Add files via upload

---
+++ app/src/static/py/Evaluaciones.py
@@ -0,0 +1,27 @@
+evaluaciones=[
+    {
+        "comentario":"gran producto, nada puede ser mejor",
+        "Calificacion":"4,5",
+        "estrellas":"5",
+        "correo":"benjaminp@uninorte.edu.co"},
+    {
+        "comentario":"Totalmente recomendado",
+        "Calificacion":"5",
+        "estrellas":"5",
+        "correo":"acurango@uninorte.edu.co"},
+    {
+        "comentario":"Muy recomendado, por mejorar el servicio postventa",
+        "Calificacion":"4",
+        "estrellas":"4",
+        "correo":"aalonsom@uninorte.edu.co"},
+    {
+        "comentario":"Muy buenos y cómodos",
+        "Calificacion":"4",
+        "estrellas":"4",
+        "correo":"pedroblanco@uninorte.edu.co"},
+    {
+        "comentario":"Gracias por la asesoría",
+        "Calificacion":"4,8",
+        "estrellas":"4",
+        "correo":"rianok@uninorte.edu.co"}
+]
---
+++ app/src/static/py/Favoritos.py
@@ -0,0 +1,10 @@
+favoritos = [
+    {
+        "id":"12345",
+        "nombre":"zapatos",
+        "precio":"120000",},
+    {
+        "id":"54321",
+        "nombre":"mini falda",
+        "precio":"80000",}      
+]
